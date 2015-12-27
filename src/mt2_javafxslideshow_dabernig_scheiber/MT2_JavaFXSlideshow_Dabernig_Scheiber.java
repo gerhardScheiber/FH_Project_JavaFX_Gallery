9644da3d7168753b6ea0ac8c245ce4b8e90c8a25
Michael Dabernig <michael.dabernig@gmail.com>
2015-12-27 17:01:14 +0100

    cleanup

--- src/mt2_javafxslideshow_dabernig_scheiber/MT2_JavaFXSlideshow_Dabernig_Scheiber.java
+++ src/mt2_javafxslideshow_dabernig_scheiber/MT2_JavaFXSlideshow_Dabernig_Scheiber.java
@@ -1,37 +1,21 @@
 package mt2_javafxslideshow_dabernig_scheiber;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.TilePane;
-import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 /**
  * @author Gerhard Scheiber
  */
 public class MT2_JavaFXSlideshow_Dabernig_Scheiber extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -44,20 +28,13 @@ public class MT2_JavaFXSlideshow_Dabernig_Scheiber extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
 
-        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent e) {
-                Platform.exit();
-                System.exit(0);
-            }
+        stage.setOnCloseRequest(event -> {
+            Platform.exit();
+            System.exit(0);
         });
 
         stage.show();
 
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 
 }
